e48eba343f0fff1f3a4b3e840643e28217611e16
Ryan McFadden <ryanmcfadden123@gmail.com>
2020-04-08 01:24:24 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,7 +19,7 @@ class Transfer
       @receiver.balance += @amount
     else
       @status = "rejected"
-      return "Transaction rejected. Please check your account balance."
+      "Transaction rejected. Please check your account balance."
     end
   end
   
