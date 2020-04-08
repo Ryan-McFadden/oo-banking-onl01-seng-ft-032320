afa694a95e48beed94a5b0c7b2a24ed8f8079e72
Ryan McFadden <ryanmcfadden123@gmail.com>
2020-04-08 01:03:58 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -6,4 +6,17 @@ class Transfer
     @sender = sender
     @reciever = reciever
     @transfer_amount = 0 
+  end
+  
+  def valid?
+    
+  end
+  
+  def execute_transaction
+    
+  end
+  
+  def reverse_transfer
+    
+  end
 end
